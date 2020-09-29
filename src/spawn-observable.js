29e30eaff89cdea7408231992c588514da971f51
Michael Gerullis <michigerullis92@gmail.com>
2020-09-29 22:02:50 +0200

    Add windows support for spawn, nump version

--- src/spawn-observable.js
+++ src/spawn-observable.js
@@ -4,23 +4,26 @@ const { Logger } = require('./logger');
 
 /**
  * Spawns a child process and returns an observable.
- * The observable completes with an error when the 
+ * The observable completes with an error when the
  * child process errors or logs to stderr.
  * It emits data logged to stdout and completes when closed.
- * @param {string} command 
- * @param {string[]} args 
- * @param {SpawnOptionsWithoutStdio} options 
+ * @param {string} command
+ * @param {string[]} args
+ * @param {SpawnOptionsWithoutStdio} options
  */
 function spawnObservable(command, args = [], options = {}) {
   const cmdStr = `${command} ${args.join(' ')}`;
   return new Observable((observer) => {
-    const spawnee = child_process.spawn(command, args, options);
+    const spawnee = child_process.spawn(command, args, {
+      ...options,
+      shell: process.platform === 'win32',
+    });
     Logger.log(`Running: ${cmdStr}`);
 
-    spawnee.stdout.on('data', data => {
+    spawnee.stdout.on('data', (data) => {
       Logger.log(`${data}`);
       observer.next(`${data}`);
-    })
+    });
 
     spawnee.stderr.on('data', (data) => {
       Logger.error(`${data}`);
@@ -33,18 +36,19 @@ function spawnObservable(command, args = [], options = {}) {
         observer.complete();
       } else {
         Logger.error(`Failed with code ${code}: ${cmdStr}`);
-        observer.error(`Failed with code ${code}: ${cmdStr}`)
+        observer.error(`Failed with code ${code}: ${cmdStr}`);
       }
     });
 
     spawnee.on('error', (error) => {
       Logger.error(error);
-      observer.error(new Error(`Child Process Error: 
+      observer.error(
+        new Error(`Child Process Error: 
       
-      ${error}`
-      ));
+      ${error}`)
+      );
     });
   });
 }
 
-module.exports = { spawnObservable }
+module.exports = { spawnObservable };
